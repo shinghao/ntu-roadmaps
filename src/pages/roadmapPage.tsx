4e2607221d929f15b655b9c453f1b3b08e6a6fa6
shinghao <sohshinghao@gmail.com>
2024-06-23 02:59:42 +0800

    feat: Change styling of select inputs to MUI

--- src/pages/roadmapPage.tsx
+++ src/pages/roadmapPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import degreeProgrammes from "../data/degreeProgrammes.json";
 import Roadmap from "@components/Roadmap";
-import LabelledSelect from "@components/Roadmap/LabelledSelect";
+import { Autocomplete, TextField } from "@mui/material";
 import "./roadmapPage.css";
 
 const typedDegreeProgrammes: {
@@ -10,45 +10,69 @@ const typedDegreeProgrammes: {
   };
 } = degreeProgrammes;
 
+const cohorts = [
+  "AY2023 & later",
+  "AY2022",
+  "AY2021",
+  "AY2020",
+  "AY2019",
+  "AY2018",
+];
+
 export default function RoadmapPage() {
-  const [degree, setDegree] = useState<string>(
-    Object.keys(typedDegreeProgrammes)[0]
-  );
+  const [degree, setDegree] = useState(Object.keys(typedDegreeProgrammes)[0]);
   const careers = typedDegreeProgrammes[degree].careers;
   const [career, setCareer] = useState<string>(careers[0] || "");
-  const cohorts = [
-    "AY2018",
-    "AY2019",
-    "AY2020",
-    "AY2021",
-    "AY2022",
-    "AY2023 & later",
+  const [cohort, setCohort] = useState(cohorts[0] || "");
+
+  const handleDegreeChange = (value: string) => {
+    setDegree(value);
+    const firstCareer = typedDegreeProgrammes[value]?.careers[0] || "";
+    setCareer(firstCareer);
+  };
+
+  const selectsConfig = [
+    {
+      options: Object.keys(degreeProgrammes),
+      label: "Degree",
+      value: degree,
+      onChange: handleDegreeChange,
+      width: 300,
+    },
+    {
+      options: careers,
+      label: "Career",
+      value: career,
+      onChange: setCareer,
+    },
+    {
+      options: cohorts,
+      label: "Cohort",
+      value: cohort,
+      onChange: setCohort,
+    },
   ];
-  const [selectedCohort, setSelectedCohort] = useState(cohorts[0] || "");
 
   return (
     <main className="content">
       <div className="selects-container">
-        <LabelledSelect
-          onChangeFn={(e) => setDegree(e.target.value)}
-          options={Object.keys(degreeProgrammes)}
-          selectName={"select-degree"}
-          label={"Degree"}
-        />
-        <LabelledSelect
-          onChangeFn={(e) => setCareer(e.target.value)}
-          options={careers}
-          selectName={"select-career"}
-          label={"Career"}
-        />
-        <LabelledSelect
-          onChangeFn={(e) => setSelectedCohort(e.target.value)}
-          options={cohorts}
-          selectName={"select-cohort"}
-          label={"Cohort"}
-        />
+        {selectsConfig.map((config) => (
+          <Autocomplete
+            key={`select-${config.label}`}
+            disablePortal
+            id={`select-${config.label}`}
+            options={config.options}
+            sx={{ width: config.width || 200 }}
+            renderInput={(params) => (
+              <TextField {...params} label={config.label} />
+            )}
+            value={config.value}
+            onChange={(_, value) => config.onChange(value)}
+            disableClearable
+          />
+        ))}
       </div>
-      <Roadmap degree={degree} career={career} cohort={selectedCohort} />
+      <Roadmap degree={degree} career={career} cohort={cohort} />
     </main>
   );
 }
