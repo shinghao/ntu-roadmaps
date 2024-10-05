5ab7d46bd64bb72d94ea28c3f554b4f255d29f93
shinghao <sohshinghao@gmail.com>
2024-10-06 01:12:08 +0800

    feat: Increase min zoom for roadmap

--- src/components/Roadmap/Roadmap.tsx
+++ src/components/Roadmap/Roadmap.tsx
@@ -73,7 +73,7 @@ const RoadmapView = ({ roadmapData }: { roadmapData: Roadmap }) => {
           nodeTypes={nodeTypes}
           zoomOnDoubleClick={false}
           fitView
-          minZoom={0.1}
+          minZoom={0.2}
         >
           <Panel position="top-right">
             <Controls
